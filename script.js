89dd56e0d9df8bafcf3cbc45862ab84303acf684
Rahul Tiwari <128848653+rrtiwari@users.noreply.github.com>
2025-08-27 09:38:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your JS code here. If required.
+let timerDisplay=document.getElementById("timer");
+let clock=new Date();
+setInterval(
+	function(){
+		clock=new Date();
+		timerDisplay.innerHTML=clock.toLocaleString();
+	},1000);
